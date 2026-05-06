a57ca4678e4b110f7826fc62a62b635d1a267f67
spinnerwheel <102970038+spinnerwheel@users.noreply.github.com>
2026-05-06 17:08:01 +0200

    Added final lab challenge files

--- vm/base.nix
+++ vm/base.nix
@@ -7,6 +7,16 @@
     };
   };
 
+  programs.bash = {
+    interactiveShellInit = ''
+      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+      then
+        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+      fi
+    '';
+  };
+
   environment.systemPackages = with pkgs; [
     btop
     curl
@@ -21,8 +31,7 @@
     wget
   ];
 
-  networking.firewall.enable  = false;
+  networking.firewall.enable = false;
 
   system.stateVersion = "25.11";
 }
-
---
+++ vm/build.fish
@@ -0,0 +1,6 @@
+function build
+    echo "Building lemon pound cake recipe..."; and \
+    sleep 5; and \
+    ffplay -loop 0 -nodisp -volume 80 '/home/uru/netsec/vm/.input' 2&>/dev/null &
+    kitten icat '/home/uru/netsec/vm/.config.gif'
+end
