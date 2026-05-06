5358286fd1b5617396c6092a201d8ac6b0750389
spinnerwheel <102970038+spinnerwheel@users.noreply.github.com>
2026-05-06 17:49:04 +0200

    Added build function

--- vm/fish.nix
+++ vm/fish.nix
@@ -16,16 +16,11 @@
           echo ✅
         end
       end
-    '';
-  };
-  environment.etc = {
-    "fish/functions/restart.fish".text = ''
-      function restart
-        for machine in $(sudo nixos-container list)
-          echo -n "[i] Restarting" $machine " "; and \
-          sudo nixos-container restart $machine; and \
-          echo ✅
-        end
+      function build
+        echo "Building lemon pound cake recipe..."; and \
+        sleep 5; and \
+        ffplay -loop 0 -nodisp -volume 80 '/home/toms/Documents/unitn/network-security/lab/vm/Never Gonna Give You Up [lYBUbBu4W08].webm' 2&>/dev/null &
+        kitten icat ~/Pictures/rick-roll.gif
       end
     '';
   };
